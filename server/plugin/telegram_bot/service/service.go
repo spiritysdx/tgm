9c5bca688b931a82609d624463480c8882d49b1b
spiritsoul <97792170+spiritysdx@users.noreply.github.com>
2024-03-14 16:42:51 +0800

    Update service.go

--- server/plugin/telegram_bot/service/service.go
+++ server/plugin/telegram_bot/service/service.go
@@ -8,21 +8,21 @@ import (
 
 type TelegramBotService struct{}
 
-func (e *TelegramBotService) SendTgMessage(token, chat_id, content, message_type string) (res string, err error) {
+func (e *TelegramBotService) SendTgMessage(token, chatId, content, messageType string) (res string, err error) {
 	bot, err := telebot.NewBot(telebot.Settings{
 		Token:  token,
 		Poller: &telebot.LongPoller{Timeout: 3 * time.Second}, // 机器人每分钟最多发送20条消息
 	})
 	if err != nil {
-		return "", err
+		return "bot initialise failure", errors.New(fmt.Sprintf("bot initialise failed: %v", err))
 	}
-	chatID, err := strconv.ParseInt(chat_id, 10, 64)
+	chatID, err := strconv.ParseInt(chatId, 10, 64)
 	if err != nil {
 		bot.Stop()
-		return "chatID cover failed", err
+		return "chatID cover failed", errors.New(fmt.Sprintf("chatID cover failed: %v", err))
 	}
 	var parseMode telebot.ParseMode
-	switch message_type {
+	switch messageType {
 	case "html":
 		parseMode = telebot.ModeHTML
 	case "markdown":
@@ -32,7 +32,7 @@ func (e *TelegramBotService) SendTgMessage(token, chat_id, content, message_type
 	}
 	_, err = bot.Send(&telebot.Chat{ID: chatID}, content, telebot.SendOptions{ParseMode: parseMode})
 	if err != nil {
-		return "telebot send message failed", err
+		return "telebot send message failed", errors.New(fmt.Sprintf("telebot send message failed: %v", err))
 	}
 	return "telebot send message success", nil
 }
